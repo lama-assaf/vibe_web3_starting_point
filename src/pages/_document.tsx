988aebc6d96f9430cf3fb1779a98879cfe1ef3e0
Lama1403 <lama@zilliqa.com>
2025-10-20 15:27:52 +0300

    Force CDN cache refresh

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body className="antialiased">
+        {/* Force CDN cache refresh */}
         <Main />
         <NextScript />
       </body>
